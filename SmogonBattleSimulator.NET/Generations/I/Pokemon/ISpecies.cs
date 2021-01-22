321c638047f947b1092ce6b3f7f05dc3d850c9df
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-01-22 23:45:15 +0100

    Add ISpecies and IType

---
+++ SmogonBattleSimulator.NET/Generations/I/Pokemon/ISpecies.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using SmogonBattleSimulator.NET.Generations.I.Type;
+
+namespace SmogonBattleSimulator.NET.Generations.I.Pokemon
+{
+    public interface ISpecies
+    {
+        string Name { get; }
+
+        int Health { get; }
+
+        int Attack { get; }
+
+        int Defense { get; }
+
+        int Special { get; }
+
+        int Speed { get; }
+
+        decimal Weight { get; }
+
+        decimal Height { get; }
+
+        IReadOnlySet<IType> Types { get; }
+    }
+}
---
+++ SmogonBattleSimulator.NET/Generations/I/Type/IType.cs
@@ -0,0 +1,13 @@
+﻿namespace SmogonBattleSimulator.NET.Generations.I.Type
+{
+    public interface IType
+    {
+        string Name { get; }
+
+        string Description { get; }
+
+        decimal GetAttackEffectiveness(IType towards);
+
+        decimal GetDefenseEffectiveness(IType against);
+    }
+}
