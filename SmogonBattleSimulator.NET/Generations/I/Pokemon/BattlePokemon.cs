779371a0df687e479566df2c94fec7b3da5dc73c
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-01-23 11:39:47 +0100

    Add BattlePokemon
    Signed-off-by: DrSmugleaf <DrSmugleaf@users.noreply.github.com>

---
+++ SmogonBattleSimulator.NET/Generations/I/Pokemon/BattlePokemon.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using SmogonBattleSimulator.NET.Generations.I.Move;
+using SmogonBattleSimulator.NET.Generations.I.Pokemon.Species;
+using SmogonBattleSimulator.NET.Generations.I.Pokemon.Species.Tier;
+using SmogonBattleSimulator.NET.Generations.I.Type;
+
+namespace SmogonBattleSimulator.NET.Generations.I.Pokemon
+{
+    public class BattlePokemon : IBattlePokemon
+    {
+        public BattlePokemon(
+            string name,
+            string nickname,
+            ISpeciesStat health,
+            ISpeciesStat attack,
+            ISpeciesStat defense,
+            ISpeciesStat special,
+            ISpeciesStat speed,
+            ISpeciesStat evasion,
+            ISpeciesStat accuracy,
+            decimal weight,
+            decimal height,
+            IReadOnlySet<IType> types,
+            IReadOnlySet<IMove> moves,
+            ITier tier)
+        {
+            Name = name;
+            Nickname = nickname;
+            Health = health;
+            Attack = attack;
+            Defense = defense;
+            Special = special;
+            Speed = speed;
+            Evasion = evasion;
+            Accuracy = accuracy;
+            Weight = weight;
+            Height = height;
+            Types = types;
+            Moves = moves;
+            Tier = tier;
+        }
+
+        public string Name { get; }
+
+        public string Nickname { get; }
+
+        public ISpeciesStat Health { get; }
+
+        public ISpeciesStat Attack { get; }
+
+        public ISpeciesStat Defense { get; }
+
+        public ISpeciesStat Special { get; }
+
+        public ISpeciesStat Speed { get; }
+
+        public ISpeciesStat Evasion { get; }
+
+        public ISpeciesStat Accuracy { get; }
+
+        public decimal Weight { get; }
+
+        public decimal Height { get; }
+
+        public IReadOnlySet<IType> Types { get; }
+
+        public IReadOnlySet<IMove> Moves { get; }
+
+        public ITier Tier { get; }
+    }
+}
---
+++ SmogonBattleSimulator.NET/Generations/I/Pokemon/IBattlePokemon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using SmogonBattleSimulator.NET.Generations.I.Move;
+using SmogonBattleSimulator.NET.Generations.I.Pokemon.Species;
+using SmogonBattleSimulator.NET.Generations.I.Pokemon.Species.Tier;
+using SmogonBattleSimulator.NET.Generations.I.Type;
+
+namespace SmogonBattleSimulator.NET.Generations.I.Pokemon
+{
+    public interface IBattlePokemon
+    {
+        string Name { get; }
+
+        string Nickname { get; }
+
+        ISpeciesStat Health { get; }
+
+        ISpeciesStat Attack { get; }
+
+        ISpeciesStat Defense { get; }
+
+        ISpeciesStat Special { get; }
+
+        ISpeciesStat Speed { get; }
+
+        ISpeciesStat Evasion { get; }
+
+        ISpeciesStat Accuracy { get; }
+
+        decimal Weight { get; }
+
+        decimal Height { get; }
+
+        IReadOnlySet<IType> Types { get; }
+
+        IReadOnlySet<IMove> Moves { get; }
+
+        ITier Tier { get; }
+    }
+}
