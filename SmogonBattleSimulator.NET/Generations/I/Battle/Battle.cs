0c41abf600c139d335fbf2bf642c5b3e252ac31f
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-01-23 15:08:55 +0100

    Add Battle class
    Signed-off-by: DrSmugleaf <DrSmugleaf@users.noreply.github.com>

---
+++ SmogonBattleSimulator.NET/Generations/I/Battle/Battle.cs
@@ -0,0 +1,15 @@
+﻿using SmogonBattleSimulator.NET.Collections.IndexedSet;
+using SmogonBattleSimulator.NET.Generations.I.Trainer;
+
+namespace SmogonBattleSimulator.NET.Generations.I.Battle
+{
+    public class Battle : IBattle
+    {
+        public Battle(IIndexedSet<ITrainer> trainers)
+        {
+            Trainers = trainers;
+        }
+
+        public IIndexedSet<ITrainer> Trainers { get; }
+    }
+}
